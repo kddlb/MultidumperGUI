a8bad045bbcad18dc8f6a832301c7354b36bc4ba
Kevin Doncam Demian López Brante <kevin@kddlb.cl>
2017-10-14 00:31:51 -0300

    New version!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -11,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace MultidumperGUI
 {
@@ -19,9 +23,245 @@ namespace MultidumperGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<int, double> _progressDictionary;
+        private bool _canceled;
+        private MDOut _songInfo;
+        private IncomingChannelInfo _channelInfo;
+        private ObservableCollection<ChannelInfo> _channelInfoParsed;
+        private System.Diagnostics.Process _infoProcess;
+        private System.Diagnostics.Process _probeProcess;
+        private System.Diagnostics.Process _dumpingProcess;
+        private OpenFileDialog _ofDialog;
+
         public MainWindow()
         {
+
             InitializeComponent();
+
+            _infoProcess = new Process()
+            {
+                EnableRaisingEvents = true
+            };
+            _probeProcess = new Process()
+            {
+                EnableRaisingEvents = true
+            };
+            _dumpingProcess = new Process()
+            {
+                EnableRaisingEvents = true
+            };
+
+            _infoProcess.OutputDataReceived += (sender, args) =>
+            {
+                if (args.Data != "" || args.Data != null)
+                {
+                    _songInfo = JsonConvert.DeserializeObject<MDOut>(args.Data);
+                    _infoProcess.CancelOutputRead();
+
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        txtGame.Text = _songInfo.Containerinfo.Game;
+                        txtSystem.Text = _songInfo.Containerinfo.System;
+                        txtDumper.Text = _songInfo.Containerinfo.Dumper;
+                        txtCopyright.Text = _songInfo.Containerinfo.Copyright;
+
+                        thrInfo.Visibility = Visibility.Hidden;
+                        thrProbe.Visibility = Visibility.Visible;
+
+                        _channelInfoParsed = new ObservableCollection<ChannelInfo>();
+
+                        Probe();
+
+
+                        var indexed = new List<IndexedSong>();
+
+                        for (int i = 0; i < _songInfo.Songs.Count; i++)
+                        {
+                            var s = _songInfo.Songs[i];
+                            s.Name = s.Name == "" ? null : s.Name;
+                            indexed.Add(new IndexedSong(i, s));
+                        }
+
+                        lstSubSongs.ItemsSource = indexed;
+
+                        
+                    });
+                }
+            };
+
+            _infoProcess.Exited += (sender, args) => { };
+            double mxp = 0.0;
+
+            _probeProcess.OutputDataReceived += (sender, args) =>
+            {
+                string line = args.Data;
+
+                if (line == null)
+                {
+                    _probeProcess.CancelOutputRead();
+                    return;
+                }
+                if (line != "")
+                {
+                    if (line[0] == 'p')
+                    {
+                        var x = line.Split('|');
+                        mxp = Double.Parse(x[2]);
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            thrProbe.Maximum = Double.Parse(x[2]);
+                            thrProbe.Value = Double.Parse(x[1]);
+                        });
+                    }
+                    else
+                    {
+                        _channelInfo = JsonConvert.DeserializeObject<IncomingChannelInfo>(line);
+                        foreach (var channel in _channelInfo.Channels)
+                        {
+                            _channelInfoParsed.Add(new ChannelInfo()
+                            {
+                                ChannelName = channel,
+                                CurrentProgress = 0.0,
+                                MaximumProgress = mxp
+                            });
+                        }
+
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            lstInfo.Visibility = Visibility.Visible;
+                            lstInfo.ItemsSource = _channelInfoParsed;
+
+                        });
+
+
+                    }
+                }
+
+
+            };
+
+            _probeProcess.Exited += (sender, args) =>
+            {
+             
+                
+            };
+
+            string lineD;
+
+            _dumpingProcess.OutputDataReceived += (sender, args) =>
+            {
+                lineD = args.Data;
+
+                if (lineD == null)
+                {
+                    _dumpingProcess.CancelOutputRead();
+                    return;
+                }
+
+                if (lineD != "")
+                {
+                    var x = lineD.Split('|');
+                    _channelInfoParsed[Int32.Parse(x[0])].MaximumProgress = Double.Parse(x[2]);
+                    _channelInfoParsed[Int32.Parse(x[0])].CurrentProgress = Double.Parse(x[1]);
+
+                }
+
+            };
+
+            _dumpingProcess.Exited += (sender, args) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    btnLoad.IsEnabled = true;
+                    btnDump.IsEnabled = true;
+
+
+                });
+                
+                Process.Start("explorer", $"/select,\"{_ofDialog.FileName}\"");
+
+            };
+        }
+
+        private void Probe()
+        {
+            _probeProcess.StartInfo = new ProcessStartInfo()
+            {
+                FileName = @"multidumper.exe",
+                Arguments = $"\"{_ofDialog.FileName}\" --probe",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            thrProbe.Visibility = Visibility.Visible;
+            lstInfo.Visibility = Visibility.Hidden;
+            _probeProcess.Start();
+            _probeProcess.BeginOutputReadLine();
+        }
+
+        private void txtFilename_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            _ofDialog = new OpenFileDialog()
+            {
+                Filter = "Supported formats|*.ay;*.gbs;*.gym;*.hes;*.kss;*.nsf;*.nsfe;*.sap;*.sfm;*.sgc;*.s" +
+                         "pc;*.spu;*.vgm;*.vgz"
+            };
+
+            if (_ofDialog.ShowDialog() != true) return;
+            txtFilename.Text = _ofDialog.FileName;
+
+            thrInfo.Visibility = Visibility.Visible;
+
+            LoadFile();
+        }
+
+        private void LoadFile()
+        {
+            _infoProcess.StartInfo = new ProcessStartInfo()
+            {
+                FileName = @"multidumper.exe",
+                Arguments = $"\"{_ofDialog.FileName}\" --json",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            btnDump.IsEnabled = false;
+
+            _infoProcess.Start();
+            _infoProcess.BeginOutputReadLine();
+        }
+
+        private void lstSubSongs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btnDump.IsEnabled = true;
+        }
+
+        private void btnDump_Click(object sender, RoutedEventArgs e)
+        {
+            btnLoad.IsEnabled = false;
+            btnDump.IsEnabled = false;
+            _dumpingProcess.StartInfo = new ProcessStartInfo()
+            {
+                FileName = @"multidumper.exe",
+                Arguments = _ofDialog.FileName.EndsWith(".spc",StringComparison.InvariantCultureIgnoreCase) ||
+                _ofDialog.FileName.EndsWith(".sfm", StringComparison.InvariantCultureIgnoreCase)
+                ? $"\"{_ofDialog.FileName}\" {lstSubSongs.SelectedIndex} {_channelInfoParsed.Count}" :
+                    $"\"{_ofDialog.FileName}\" {lstSubSongs.SelectedIndex}",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+
+            };
+            _dumpingProcess.Start();
+            _dumpingProcess.BeginOutputReadLine();
+
         }
     }
 }
---
+++ MultidumperJSONInfo.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MultidumperGUI.Annotations;
+using Newtonsoft.Json;
+
+namespace MultidumperGUI
+{
+    public class Containerinfo
+    {
+        [JsonProperty("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonProperty("dumper")]
+        public string Dumper { get; set; }
+
+        [JsonProperty("game")]
+        public string Game { get; set; }
+
+        [JsonProperty("system")]
+        public string System { get; set; }
+    }
+
+    public class Song
+
+    {
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class IndexedSong:Song
+
+    {
+        public IndexedSong(int id, Song baseSong)
+        {
+            Id = id;
+            Author = baseSong.Author;
+            Comment = baseSong.Comment;
+            Name = baseSong.Name;
+        }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        
+
+
+        
+    }
+
+    public class IncomingChannelInfo
+    {
+        [JsonProperty("channels")]
+        public IList<string> Channels { get; set; }
+
+
+    }
+
+    public class ChannelInfo : INotifyPropertyChanged
+    {
+        private string _channelName;
+        private double _maximumProgress;
+        private double _currentProgress;
+
+        public string ChannelName
+        {
+            get { return _channelName; }
+            set { _channelName = value; }
+        }
+
+        public double MaximumProgress
+        {
+            get { return _maximumProgress; }
+            set { _maximumProgress = value; OnPropertyChanged(nameof(MaximumProgress)); }
+        }
+
+        public double CurrentProgress
+        {
+            get { return _currentProgress; }
+            set { _currentProgress = value; OnPropertyChanged(nameof(CurrentProgress)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class MDOut
+    {
+      
+        [JsonProperty("containerinfo")]
+        public Containerinfo Containerinfo { get; set; }
+
+        [JsonProperty("songs")]
+        public IList<Song> Songs { get; set; }
+
+        [JsonProperty("subsongCount")]
+        public int SubsongCount { get; set; }
+    }
+}
